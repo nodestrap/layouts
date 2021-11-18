9d53d4eaa3028e5cb7f49a067e43f17e1b613e2d
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2021-11-19 02:40:38 +0700

    converted to es6 module

--- dist/layouts.js
+++ dist/layouts.js
@@ -1,14 +1,15 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fillTextLineWidthLayout = exports.fillTextLineHeightLayout = void 0;
 // cssfn:
-const cssfn_1 = require("@cssfn/cssfn"); // cssfn core
+import { 
+// compositions:
+composition, imports, 
+// layouts:
+layout, } from '@cssfn/cssfn'; // cssfn core
 /**
  * A dummy text content, for making parent's height as tall as current text line-height.
  * The dummy is also used for calibrating the flex's vertical position.
  */
-const fillTextLineHeightLayout = () => (0, cssfn_1.composition)([
-    (0, cssfn_1.layout)({
+export const fillTextLineHeightLayout = () => composition([
+    layout({
         // layouts:
         content: '"\xa0"',
         display: 'inline-block',
@@ -19,20 +20,18 @@ const fillTextLineHeightLayout = () => (0, cssfn_1.composition)([
         inlineSize: 0, // kill the width, we just need the height
     }),
 ]);
-exports.fillTextLineHeightLayout = fillTextLineHeightLayout;
 /**
  * A dummy text content, for making parent's width as wide as current text line-height.
  * The dummy is also used for calibrating the flex's vertical position.
  */
-const fillTextLineWidthLayout = () => (0, cssfn_1.composition)([
-    (0, cssfn_1.imports)([
-        (0, exports.fillTextLineHeightLayout)(),
+export const fillTextLineWidthLayout = () => composition([
+    imports([
+        fillTextLineHeightLayout(),
     ]),
-    (0, cssfn_1.layout)({
+    layout({
         // layouts:
         writingMode: 'vertical-lr',
         // appearances:
         overflow: 'unset', // fix Firefox bug
     }),
 ]);
-exports.fillTextLineWidthLayout = fillTextLineWidthLayout;
